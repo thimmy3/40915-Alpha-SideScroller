bd042ed810106d90272f849207b5db047cc668e6
Stormpunk <andjkim1@gmail.com>
2021-03-11 15:11:46 +1100

    Initial Commit of Enemy Scripts
    Iteration 1 of enemy scripts:
    - Patrolling Ground Unit: Functional
    - Stationary Turret: Semi Functional- No maximum range for projectile spawn.
    - Flying Unit: Hitbox error with Player

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public GameObject thePlatform;
+    public Transform generationPoint;
+    public float distanceBetween;
+
+    private float platformWidth;
+
+    private void Start()
+    {
+        platformWidth = thePlatform.GetComponent<BoxCollider2D>().size.x;
+    }
+    private void Update()
+    {
+        if(transform.position.x < generationPoint.position.x)
+        {
+            transform.position = new Vector3(transform.position.x + platformWidth + distanceBetween, transform.position.y, transform.position.z);
+
+            Instantiate(thePlatform, transform.position, transform.rotation);
+        }
+    }
+}
